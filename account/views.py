fe4ead93ca67d863dc07ad907577c40bb82fbc32
Ananno Barua <anannobarua@gmail.com>
2023-10-25 12:32:31 +0600

    Change date oct-25

--- account/views.py
+++ account/views.py
@@ -4,6 +4,16 @@ from .forms import CreateUserForm
 
 
 
+from django.contrib.auth import get_current_site
+from . token import user_tokenizer_generate
+
+
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
+
+
 # Create your views here.
 
 def register(request):
@@ -16,9 +26,35 @@ def register(request):
         
         if form.is_valid():
             
-            form.save()
+            # form.save()
             
-            return redirect('store')
+            # return redirect('store')
+            
+            user = form.save()
+            
+            user.is_activee = False
+            
+            user.save()
+            
+            # Email verification setup (template)
+            
+            current_site = get_current_site(request)
+            
+            subject = 'Account verification email'
+            
+            message = render_to_string('account/registration/email-verification.html', {
+                                
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+                            
+            })
+            
+            user.email_user(subject=subject, message=message)
+            
+            
+            return redirect('email-verification-sent')
         
     context = {'form': form}
     
